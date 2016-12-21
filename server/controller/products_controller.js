7bd1bca36411486b04bb6babdde256db11b4e4cc
佑佑科技 <apple2@ioioinfo.com>
2016-12-21 17:04:34 +0800

    产品页面信息功能

--- server/controller/products_controller.js
+++ server/controller/products_controller.js
@@ -56,8 +56,37 @@ var find_samll_picture = function(same_product_ids, cb){
 	url = url + JSON.stringify(same_product_ids);
 	do_get_method(url,cb);
 };
+//根据id找到商品所有评论数量
+var find_total_comments = function(product_id, cb){
+	var url = "http://139.196.148.40:16001/get_product_comments_summary?product_id=";
+	url = url + product_id;
+	do_get_method(url,cb);
+};
+//根据id找到评论
+var find_comments_info = function(product_id, cb){
+	var url = "http://127.0.0.1:8060/comments_show?product_id=";
+	url = url + product_id;
+	do_get_method(url,cb);
+};
+//根据personid找到人
+var find_comments_persons = function(comments_persons, cb){
+	var url = "http://127.0.0.1:8060/comments_persons?person_ids=";
+	url = url + comments_persons;
+	do_get_method(url,cb);
+};
+//根据评论找晒单
+var find_saidans_pictures = function(comments_ids, cb){
+	var url = "http://127.0.0.1:8060/comments_saidan?comments_ids=";
+	url = url + comments_ids;
+	do_get_method(url,cb);
+};
+//根据产品id找产品详细
+var find_product_details = function(product_id, cb){
+	var url = "http://127.0.0.1:7000/get_product_details?product_id=";
+	url = url + product_id;
+	do_get_method(url,cb);
+};
 exports.register = function(server, options, next){
-
 	server.route([
 		{
 			method: 'GET',
@@ -78,9 +107,9 @@ exports.register = function(server, options, next){
 						var industry = industries[industry_id];
 						var stock_options = {"region_id":"1"};
 
-						var ep =  eventproxy.create("pictures","industry_info","stock","sales","same_pictures",
-							function(pictures,industry_info,stock,sales,same_pictures){
-							return reply.view(industry["view_name"],{"product":product,"pictures":pictures,"industry":industry,"industry_info":industry_info,"stock":stock,"sales":sales,"same_pictures":same_pictures})
+						var ep =  eventproxy.create("pictures","industry_info","stock","sales","same_pictures","total_comments","comments","persons","saidans","product_details",
+							function(pictures,industry_info,stock,sales,same_pictures,total_comments,comments,persons,saidans,product_details){
+							return reply.view(industry["view_name"],{"product":product,"pictures":pictures,"industry":industry,"industry_info":industry_info,"stock":stock,"sales":sales,"same_pictures":same_pictures,"total_comments":total_comments,"comments":comments,"persons":persons,"saidans":saidans,"product_details":product_details})
 						});
 						find_industry(industry_id,product_id, function(err, content){
 							if (!err) {
@@ -107,6 +136,12 @@ exports.register = function(server, options, next){
 								ep.emit("sales", sales);
 							}
 						});
+						find_total_comments(product_id, function(err, content){
+							if (!err) {
+								var total_comments = content.row;
+								ep.emit("total_comments", total_comments);
+							}
+						});
 						find_same_products(product_id, same_code, function(err, content){
 							if (!err) {
 								var same_products = content.rows;
@@ -130,7 +165,69 @@ exports.register = function(server, options, next){
 								}
 							}
 						});
+						find_comments_info(product_id, function(err, content){
+							if (!err) {
+								var comments = content.rows;
+								var comments_ids = [];
+								var comments_persons = [];
+								if (comments) {
+									for (var i = 0; i < comments.length; i++) {
+										comments_ids.push(comments[i].id);
+										comments_persons.push(comments[i].person_id);
+									}
+									comments_persons = JSON.stringify(comments_persons);
+									comments_ids = JSON.stringify(comments_ids);
+									var eproxy =  eventproxy.create("persons","saidans",
+										function(persons,saidans){
+											ep.emit("comments", comments);
+											var person_map = {};
+											var saidan_map = {};
+											for (var i = 0; i < persons.length; i++) {
+												person_map[persons[i].id] = persons[i];
+											}
+											for (var i = 0; i < saidans.length; i++) {
+												var saidan = saidans[i]; //一个晒单对象
+												var comment_saidans = [];
+												if (saidan_map[saidan.product_comments_id]) { //晒单评论id对应的不存在
+													comment_saidans = saidan_map[saidan.product_comments_id]; //晒单 = 晒单产品id
+												}
+												comment_saidans.push(saidan); //晒单添加 晒单对象
+												saidan_map[saidan.product_comments_id] = comment_saidans; //晒单评论id = 晒单
+											}
+											ep.emit("persons", person_map);
+											ep.emit("saidans", saidan_map);
+									});
+									find_comments_persons(comments_persons, function(err, content){
+										if (!err) {
+											var persons = content.rows;
+											eproxy.emit("persons", persons);
+										}else {
+											eproxy.emit("persons", []);
+										}
+									});
+									find_saidans_pictures(comments_ids, function(err, content){
+										if (!err) {
+											var saidans = content.rows;
+											eproxy.emit("saidans", saidans);
+										}else {
+											eproxy.emit("saidans", []);
+										}
+									});
+								}else {
+									ep.emit("comments", []);
+								}
+							}
+						});
+						find_product_details(product_id, function(err, content){
+							if (!err) {
+								var  product_details = content.rows;
+								ep.emit("product_details", product_details);
+							}else {
+								ep.emit("product_details", []);
+							}
+						});
 					}else {
+						return reply("404");
 					}
 				});
 			}
